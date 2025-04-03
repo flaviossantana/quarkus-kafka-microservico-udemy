2ad21acdd41f402e16783984e9632b0814ed9e8b
flavio.santana <9090flavio@gmail.com>
2025-04-03 18:57:56 -0300

    cotacao-mineradora: implementando o agendamento do JOB de cotação

---
+++ cotacao-mineradora/src/main/java/org/br/mineradora/scheduler/QuotationScheduler.java
@@ -0,0 +1,29 @@
+package org.br.mineradora.scheduler;
+
+import io.quarkus.scheduler.Scheduled;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.br.mineradora.service.QuotationService;
+
+@Slf4j
+@ApplicationScoped
+public class QuotationScheduler {
+
+    @Inject
+    QuotationService quotationService;
+
+    @Transactional
+    @Scheduled(every = "35s", identity = "task-quotation-scheduler")
+    public void schedule() {
+
+        log.info("INICIANDO A EXECUÇÃO DE BUSCA DE COTAÇÃO USD-BRL");
+
+        quotationService.getCurrencyPrice();
+
+        log.info("FINALIZANDO A EXECUÇÃO DE BUSCA DE COTAÇÃO USD-BRL");
+    }
+
+
+}
