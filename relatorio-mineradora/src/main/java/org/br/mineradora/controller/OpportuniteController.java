dcf7336742e18a51baa01dbe7a6b558ef22aeaf5
flavio.santana <9090flavio@gmail.com>
2025-04-09 18:05:25 -0300

    relatorio-mineradora: implementação a api de geração do CSV

--- relatorio-mineradora/src/main/java/org/br/mineradora/controller/OpportuniteController.java
+++ relatorio-mineradora/src/main/java/org/br/mineradora/controller/OpportuniteController.java
@@ -1,21 +1,38 @@
 package org.br.mineradora.controller;
 
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.br.mineradora.service.OpportunityService;
+
+import java.util.Date;
 
 @Slf4j
 @RequestScoped
 @Path("/api/opportunity")
 public class OpportuniteController {
 
+    @Inject
+    OpportunityService opportunityService;
+
     @GET
-    @Path("/{id}")
-    public Response find(@PathParam("id") long id) {
-        return Response.ok(id).build();
+    @Path("/report")
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response generateReport() {
+        try {
+            return Response
+                    .ok(opportunityService.generateOpportunitesData(), MediaType.APPLICATION_OCTET_STREAM)
+                    .header("Content-Disposition", String.format("attachment; filename=%s-opportunities-sales.txt", new Date()))
+                    .build();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return Response.serverError().build();
+        }
     }
 
 }
