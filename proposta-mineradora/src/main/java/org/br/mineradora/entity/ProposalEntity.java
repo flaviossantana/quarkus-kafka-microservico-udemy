c4841755905f6053a7424255e65dca95827e43d4
flavio.santana <9090flavio@gmail.com>
2025-04-09 14:28:27 -0300

    proposta-mineradora: criando entidade

---
+++ proposta-mineradora/src/main/java/org/br/mineradora/entity/ProposalEntity.java
@@ -0,0 +1,34 @@
+package org.br.mineradora.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "proposal")
+public class ProposalEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "price_tonne")
+    private BigDecimal priceTonne;
+
+    @Column(name = "proprosal_valid_days")
+    private Integer proprosalValidDays;
+
+    private String costumer;
+    private Integer tonnes;
+    private String country;
+
+}
